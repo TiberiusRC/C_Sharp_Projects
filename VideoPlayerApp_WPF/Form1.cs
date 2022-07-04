a2c993449929a0dca901fd2a5c7c3107ba9fbe25
Ronald Cober <Ronald.cober.bee@outlook.com>
2022-07-04 16:36:13 +0200

    Added variables  and  LoadFolderEvent

--- VideoPlayerApp_WPF/Form1.cs
+++ VideoPlayerApp_WPF/Form1.cs
@@ -7,12 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+// Added IO , to access external files for video player.
 using System.IO;
 
 namespace VideoPlayerApp_WPF
 {
     public partial class Form1 : Form
     {
+        //To store the video file.
+        List<string> filteredFiles = new List<string>();
+        // To show the browsable files. 
+        FolderBrowserDialog browser = new FolderBrowserDialog();
+        // To show which file is currently playing.
+        int currentFile = 0;
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +27,27 @@ namespace VideoPlayerApp_WPF
 
         private void loadFolderEvent(object sender, EventArgs e)
         {
+            // Stop any playing file and clearing the list.
+            VideoPlayer.Ctlcontrols.stop();
+            if(filteredFiles.Count > 1)
+            {
+                filteredFiles.Clear();
+                filteredFiles = null;
 
+                Playlist.Items.Clear();
+
+                currentFile = 0;
+            }
+
+            // Any file with the correct extension will be added to the playlist.
+            DialogResult result = browser.ShowDialog();
+            
+            if (result == DialogResult.OK)
+            {
+                filteredFiles = Directory.GetFiles(browser.SelectedPath, "*.*").Where(file => file.ToLower().EndsWith("webm") || file.ToLower().EndsWith("mp4") || file.ToLower().EndsWith("wmv") || file.ToLower().EndsWith("mkv") || file.ToLower().EndsWith("avi")).ToList();
+
+                LoadPlayList();
+            }
         }
 
         private void ShowAboutEvent(object sender, EventArgs e)
